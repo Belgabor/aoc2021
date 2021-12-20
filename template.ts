5416d26213b22416cac4bdf5d849a85cb960b26f
Belgabor <belgabor@gmx.de>
2021-12-20 14:05:31 +0100

    Template/Lib

--- template.ts
+++ template.ts
@@ -1,6 +1,6 @@
 // deno-lint-ignore-file camelcase ban-unused-ignore
 export { }
-const test = false
+const test = true
 const data = await Deno.readTextFile(test ? './test_data.txt' : './data.txt')
 const lines = data.split(/\r?\n/)
 console.log(lines)
