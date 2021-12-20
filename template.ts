111e732ff04772f5c2f21d7dcbe4dab845acfad8
Belgabor <belgabor@gmx.de>
2021-12-20 13:26:42 +0100

    Init Script

---
+++ template.ts
@@ -0,0 +1,7 @@
+// deno-lint-ignore-file camelcase ban-unused-ignore
+export { }
+const test = false
+const data = await Deno.readTextFile(test ? './test_data.txt' : './data.txt')
+const lines = data.split(/\r?\n/)
+console.log(lines)
+
